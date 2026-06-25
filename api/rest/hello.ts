8b029a4f941d5868797cb060845c45c9f45decf9
arthurwerle <arthur.werle@gmail.com>
2026-06-25 08:21:17 -0300

    add deno

--- api/rest/hello.ts
+++ api/rest/hello.ts
@@ -1,9 +1,9 @@
 import { FastifyInstance } from "fastify/types/instance";
 
-async function routes (fastify: FastifyInstance) {
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-  })
+async function routes(fastify: FastifyInstance) {
+  fastify.get("/", async (request, reply) => {
+    return { hello: "world" };
+  });
 }
 
 export default routes;
--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-import Fastify from 'fastify'
-import hello from './api/rest/hello'
+import Fastify from "fastify";
+import hello from "./api/rest/hello";
 
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
 
-fastify.register(hello)
+fastify.register(hello);
 
 fastify.listen({ port: 3000 }, function (err, address) {
   if (err) {
-    fastify.log.error(err)
+    fastify.log.error(err);
   }
-})
+});
